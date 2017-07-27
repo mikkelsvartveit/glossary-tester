656cd74c3b760aba3b1f576d5c7e38e0375c6317
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2017-07-27 22:00:40 +0200

    bug fix

--- de/tests/index.php
+++ de/tests/index.php
@@ -35,7 +35,7 @@
 
                 <h2>Letzte test-Ergebnis</h2>
                 
-                <p id="lastTestComment">In deine letzten test, hast du <span id="correctWords"></span> von <span id="totalWords"></span> richtigen Worte. <a href="/Tysk/tests/last-test" class="link">Details anzeigen.</a>.</p>
+                <p id="lastTestComment">In deine letzten test, hast du <span id="correctWords"></span> von <span id="totalWords"></span> richtigen Worte. <a href="/de/tests/last-test" class="link">Details anzeigen.</a>.</p>
                 <p id="noTestComment">Du nicht hast abgeschlossen jeder tests noch. Wenn du einen test nehmen, das Ergebnis wird zeigen, bis hier.</p>
 
             </div>
@@ -44,7 +44,7 @@
 
                 <h2>Neuer test</h2>
                 <p>Klicken Sie auf die Schaltfläche starten, um ein neues Glossar zu testen.</p>
-                <button class="center" onclick="location.href='/Tysk/tests/take-test';">Starten einen neuen test</button>
+                <button class="center" onclick="location.href='/de/tests/take-test';">Starten einen neuen test</button>
 
             </div>
         
