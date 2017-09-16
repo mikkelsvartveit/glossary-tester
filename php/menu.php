472b2646963f6a33e8aeddce9cb31a1f5dd2b242
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2017-09-16 19:26:57 +0200

    updated menu bar

--- php/menu.php
+++ php/menu.php
@@ -1,11 +1,10 @@
 <div class="menu">
     
     <div class="menu-group menu-left">
-        
+        <a href="/">Glossary Tester</a>
     </div>
 
     <div class="menu-group menu-center">
-        <a href="/">Home</a>
         <a href="/manage">Manage</a>
         <a href="/practice">Practice</a>
         <a href="/tests">Test</a>
