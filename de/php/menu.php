32f6bb58129b4d3f1838f11a6bfbb7cc23de5baf
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2018-02-16 22:44:35 +0100

    removed obsolete german version

--- de/php/menu.php
+++
@@ -1,8 +0,0 @@
-<div class="menu">
-
-    <a href="/de"><img src="/img/home.png"></a>
-    <a href="/de/manage"><img src="/img/manage.png"></a>
-    <a href="/de/practice"><img src="/img/practice.png"></a>
-    <a href="/de/tests"><img src="/img/test.png"></a>
-
-</div>
